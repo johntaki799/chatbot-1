d837f2891d4b700c72fcc2231bda33c10e758be0
abdullah-manafikhi <abdullahmanafikhi32@gmail.com>
2023-05-09 16:16:57 +0300

    lstening for a message from the linked page

--- controllers/chatbotControllers.js
+++ controllers/chatbotControllers.js
@@ -38,6 +38,18 @@ const postWebhook = (req, res) => {
     body.entry.forEach((entry) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
