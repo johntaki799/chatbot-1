5026002ab9a75b25364686d48bce3a10e584b37b
abdullah-manafikhi <abdullahmanafikhi32@gmail.com>
2023-05-09 17:49:27 +0300

    troubleshoot-2

--- controllers/chatbotControllers.js
+++ controllers/chatbotControllers.js
@@ -11,15 +11,17 @@ const getWebhook = (req, res) => {
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
-
+  console.log("1")
   // Check if a token and mode is in the query string of the request
   if (mode && token) {
+    console.log("2")
     // Check the mode and token sent is correct
     if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Respond with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
+      console.log("4")
       // Respond with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
