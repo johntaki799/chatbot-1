43c2bcb83725c6af67d4dd5308751672183e1f32
abdullah-manafikhi <abdullahmanafikhi32@gmail.com>
2023-05-10 12:41:15 +0300

    finally

--- controllers/chatbotControllers.js
+++ controllers/chatbotControllers.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const request = require('request');
 
 const test = (req, res) => {
   res.send("hello from the chatbot controller");
@@ -11,37 +12,37 @@ const getWebhook = (req, res) => {
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
-  console.log("1")
+  console.log("1");
   // Check if a token and mode is in the query string of the request
-    console.log("2")
-    // Check the mode and token sent is correct
-    if (mode === "subscribe" && token) {
-      // Respond with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      console.log("4")
-      // Respond with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
+  console.log("2");
+  // Check the mode and token sent is correct
+  if (mode === "subscribe" && token) {
+    // Respond with the challenge token from the request
+    console.log("WEBHOOK_VERIFIED");
+    res.status(200).send(challenge);
+  } else {
+    console.log("4");
+    // Respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
 };
 
 const postWebhook = (req, res) => {
   let body = req.body;
-
+  console.lof(process.env.MY_VERIFY_TOKEN)
   console.log(`\u{1F7EA} Received webhook:`);
   console.dir(body, { depth: null });
 
   // Send a 200 OK response if this is a page webhook
 
   if (body.object === "page") {
-    console.log("fuck")
+    console.log("fuck");
     body.entry.forEach((entry) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
       // Get the sender PSID
-      let sender_psid = webhook_event.sender;
+      let sender_psid = webhook_event.sender.id;
       // console.log("Sender PSID: " + sender_psid);
 
       // Check if the event is a message or postback and
@@ -64,13 +65,48 @@ const postWebhook = (req, res) => {
 };
 
 // Handles messages events
-function handleMessage(sender_psid, received_message) {}
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {}
+function callSendAPI(sender_psid, response) {
+   // Construct the message body
+   let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
 
 module.exports = {
   test,
